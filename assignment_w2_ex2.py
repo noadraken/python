4e171150319742d3bd15513991e7a6c1c9c93410
noadraken <aungoo.ti25@nusaputra.ac.id>
2026-03-01 14:08:17 +0700

    Assignment 2 Update

--- assignment_w2_ex2.py
+++ assignment_w2_ex2.py
@@ -1,3 +1,15 @@
+working_minutes = 45
+working_hours = 5
+convert_into_hours = working_hours + (working_minutes/60)
+hourly_rate = 85000
+
+total_payment = hourly_rate * convert_into_hours
+print("The total payment is ", total_payment)
+
+print("\n\n")
+
+
+
 base_salary = 500000
 allowance = 750000
 bpjs = 0.02
