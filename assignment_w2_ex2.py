6aa2365da84327bf5a25ff596409129f63df32f6
noadraken <aungoo.ti25@nusaputra.ac.id>
2026-03-02 12:47:57 +0700

    Assignment 2 Update

--- assignment_w2_ex2.py
+++ assignment_w2_ex2.py
@@ -4,9 +4,26 @@ convert_into_hours = working_hours + (working_minutes/60)
 hourly_rate = 85000
 
 total_payment = hourly_rate * convert_into_hours
-print("The total payment is ", total_payment)
+print("The total payment is ", total_payment, " per day")
+
+income_per_month = total_payment * 30
+
+
+laptop_price = 1400000
+
+months_required = laptop_price / income_per_month
+
+if laptop_price%income_per_month>0:
+    months_required =+1
+else:
+    months_required
+    
+print("If he collect his daily income, his monthly income will be ", income_per_month, ". He needs to collect ", months_required, "months worth of income to be able to afford a new laptop.")
 
 print("\n\n")
+
+
+
 
 
 
